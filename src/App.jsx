329f2d051775f91e195fd360498db49d268b41c1
pankaj297 <pankajnaik297@gmail.com>
2025-08-24 17:35:21 +0530

    abc

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-// App.jsx
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/HomePage.jsx";
 import { AdminLogin } from "./components/AdminLogin.jsx";
 import AdminHome from "./components/AdminHome.jsx";
@@ -8,19 +7,24 @@ import RegisterForm from "./components/RegisterForm.jsx";
 import AdminFullProfile from "./components/AdminFullProfile.jsx";
 import Update from "./components/Update.jsx";
 
+
 import "./styles/global.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/register" element={<RegisterForm />} />
-      <Route path="/admin-login" element={<AdminLogin />} />
-      <Route path="/cbaddda" element={<AdminHome />} />
-      <Route path="/profile/:id" element={<UserProfile />} />
-      <Route path="/update/:id" element={<Update />} />
-      <Route path="/cbaddda/user/:id" element={<AdminFullProfile />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="admin-login" element={<AdminLogin />} />
+        <Route path="/cbaddda" element={<AdminHome />} />
+        {/* Regular user profile */}
+        <Route path="/profile/:id" element={<UserProfile />} />
+        <Route path="/update/:id" element={<Update />} /> {/* 🔹 Added */}
+        {/* Admin full profile */}
+        <Route path="/cbaddda/user/:id" element={<AdminFullProfile />} />
+      </Routes>
+    </Router>
   );
 }
 
