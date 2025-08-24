d9bc39a0ccf04928f21db7e82a208d824fff6f67
pankaj297 <pankajnaik297@gmail.com>
2025-08-24 17:11:07 +0530

    double router error solove

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// App.jsx
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/HomePage.jsx";
 import { AdminLogin } from "./components/AdminLogin.jsx";
 import AdminHome from "./components/AdminHome.jsx";
@@ -7,24 +8,19 @@ import RegisterForm from "./components/RegisterForm.jsx";
 import AdminFullProfile from "./components/AdminFullProfile.jsx";
 import Update from "./components/Update.jsx";
 
-
 import "./styles/global.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="admin-login" element={<AdminLogin />} />
-        <Route path="/cbaddda" element={<AdminHome />} />
-        {/* Regular user profile */}
-        <Route path="/profile/:id" element={<UserProfile />} />
-        <Route path="/update/:id" element={<Update />} /> {/* 🔹 Added */}
-        {/* Admin full profile */}
-        <Route path="/cbaddda/user/:id" element={<AdminFullProfile />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<RegisterForm />} />
+      <Route path="/admin-login" element={<AdminLogin />} />
+      <Route path="/cbaddda" element={<AdminHome />} />
+      <Route path="/profile/:id" element={<UserProfile />} />
+      <Route path="/update/:id" element={<Update />} />
+      <Route path="/cbaddda/user/:id" element={<AdminFullProfile />} />
+    </Routes>
   );
 }
 
